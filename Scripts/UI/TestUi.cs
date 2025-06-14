947b917b4837c829868f7f5209cbceec48558871
Puma008 <markus.sandmann-9999@web.de>
2025-06-14 18:02:13 +0200

    FPS Counter

--- Scripts/UI/TestUi.cs
+++ Scripts/UI/TestUi.cs
@@ -6,45 +6,49 @@ using TeroftheMagic.Scripts.Utility;
 
 public partial class TestUi : Control {
 
-    Label PlayerPosX;
-    Label PlayerPosY;
-    Label MousePosX;
-    Label MousePosY;
-    Label CanPosX;
-    Label CanPosY;
+	Label PlayerPosX;
+	Label PlayerPosY;
+	Label MousePosX;
+	Label MousePosY;
+	Label CanPosX;
+	Label CanPosY;
+	Label FPSCounter;
 
-    public override void _Ready() {
-        base._Ready();
+	public override void _Ready() {
+		base._Ready();
 
 
-        PlayerPosX = GetNode<Label>("BG/VBoxContainer/PPX");
-        PlayerPosY = GetNode<Label>("BG/VBoxContainer/PPY");
-        MousePosX = GetNode<Label>("BG/VBoxContainer/MPX");
-        MousePosY = GetNode<Label>("BG/VBoxContainer/MPY");
-        CanPosX = GetNode<Label>("BG/VBoxContainer/CPX");
-        CanPosY = GetNode<Label>("BG/VBoxContainer/CPY");
+		PlayerPosX = GetNode<Label>("BG/VBoxContainer/PPX");
+		PlayerPosY = GetNode<Label>("BG/VBoxContainer/PPY");
+		MousePosX = GetNode<Label>("BG/VBoxContainer/MPX");
+		MousePosY = GetNode<Label>("BG/VBoxContainer/MPY");
+		CanPosX = GetNode<Label>("BG/VBoxContainer/CPX");
+		CanPosY = GetNode<Label>("BG/VBoxContainer/CPY");
+		FPSCounter = GetNode<Label>("BG/VBoxContainer/FPS");
 
-    }
+	}
 
-    public override void _Process(double delta) {
-        base._Process(delta);
+	public override void _Process(double delta) {
+		base._Process(delta);
 
-        if (!loaded) return;
+		if (!loaded) return;
 
-        Vector2 canvasPos = GetLocalMousePosition();
-        CanPosX.Text = $"Canvas Pos x: [ {canvasPos.X} ]";
-        CanPosY.Text = $"Canvas Pos y: [ {canvasPos.Y} ]";
+		Vector2 canvasPos = GetLocalMousePosition();
+		CanPosX.Text = $"Canvas Pos x: [ {canvasPos.X} ]";
+		CanPosY.Text = $"Canvas Pos y: [ {canvasPos.Y} ]";
 
-        Vector2 mousePos = World.Main.ToGlobal(World.Main.GetLocalMousePosition());
-        Vector2I mapPos = new((int)(mousePos.X / 16), (int)(mousePos.Y / 16));
-        MousePosX.Text = $"Mouse Pos x: [ {mapPos.X} ]";
-        MousePosY.Text = $"Mouse Pos y: [ {mapPos.Y *= -1} ]";
+		Vector2 mousePos = World.Main.GetLocalMousePosition();
+		Vector2I mapPos = new((int)(mousePos.X / 16), (int)(mousePos.Y / 16));
+		MousePosX.Text = $"Mouse Pos x: [ {mapPos.X} ]";
+		MousePosY.Text = $"Mouse Pos y: [ {mapPos.Y *= -1} ]";
 
-        Vector2 playerPos = PlayerMovement.PlayerPosition / 16;
-        PlayerPosX.Text = $"Player Pos x: [ {playerPos.X} ]";
-        PlayerPosY.Text = $"Player Pos y: [ {playerPos.Y *= -1} ]";
+		Vector2 playerPos = PlayerMovement.PlayerPosition / 16;
+		PlayerPosX.Text = $"Player Pos x: [ {playerPos.X} ]";
+		PlayerPosY.Text = $"Player Pos y: [ {playerPos.Y *= -1} ]";
 
-    }
+		FPSCounter.Text = $"FPS: [ {Engine.GetFramesPerSecond()} ]";
+
+	}
 
 
 }
